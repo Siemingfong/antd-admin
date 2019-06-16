da6d04f13760097fe66429a53739bc325e22b063
MingFong <sfe3015@gmail.com>
2019-06-16 04:44:49 -0500

    Reduce elements

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -6,15 +6,9 @@ import { Color } from 'utils'
 import { Page, ScrollBar } from 'components'
 import {
   NumberCard,
-  Quote,
-  Sales,
-  Weather,
   RecentSales,
   Comments,
   Completed,
-  Browser,
-  Cpu,
-  User,
 } from './components'
 import styles from './index.less'
 
@@ -33,18 +27,12 @@ const bodyStyle = {
 }))
 class Dashboard extends PureComponent {
   render() {
-    const { avatar, username, dashboard, loading } = this.props
+    const { dashboard, loading } = this.props
     const {
-      weather,
-      sales,
-      quote,
       numbers,
       recentSales,
       comments,
       completed,
-      browser,
-      cpu,
-      user,
     } = dashboard
 
     const numberCards = numbers.map((item, key) => (
@@ -59,52 +47,6 @@ class Dashboard extends PureComponent {
         className={styles.dashboard}
       >
         <Row gutter={24}>
-          {numberCards}
-          <Col lg={18} md={24}>
-            <Card
-              bordered={false}
-              bodyStyle={{
-                padding: '24px 36px 24px 0',
-              }}
-            >
-              <Sales data={sales} />
-            </Card>
-          </Col>
-          <Col lg={6} md={24}>
-            <Row gutter={24}>
-              <Col lg={24} md={12}>
-                <Card
-                  bordered={false}
-                  className={styles.weather}
-                  bodyStyle={{
-                    padding: 0,
-                    height: 204,
-                    background: Color.blue,
-                  }}
-                >
-                  <Weather
-                    {...weather}
-                    loading={loading.effects['dashboard/queryWeather']}
-                  />
-                </Card>
-              </Col>
-              <Col lg={24} md={12}>
-                <Card
-                  bordered={false}
-                  className={styles.quote}
-                  bodyStyle={{
-                    padding: 0,
-                    height: 204,
-                    background: Color.peach,
-                  }}
-                >
-                  <ScrollBar>
-                    <Quote {...quote} />
-                  </ScrollBar>
-                </Card>
-              </Col>
-            </Row>
-          </Col>
           <Col lg={12} md={24}>
             <Card bordered={false} {...bodyStyle}>
               <RecentSales data={recentSales} />
@@ -125,26 +67,6 @@ class Dashboard extends PureComponent {
               }}
             >
               <Completed data={completed} />
-            </Card>
-          </Col>
-          <Col lg={8} md={24}>
-            <Card bordered={false} {...bodyStyle}>
-              <Browser data={browser} />
-            </Card>
-          </Col>
-          <Col lg={8} md={24}>
-            <Card bordered={false} {...bodyStyle}>
-              <ScrollBar>
-                <Cpu {...cpu} />
-              </ScrollBar>
-            </Card>
-          </Col>
-          <Col lg={8} md={24}>
-            <Card
-              bordered={false}
-              bodyStyle={{ ...bodyStyle.bodyStyle, padding: 0 }}
-            >
-              <User {...user} avatar={avatar} username={username} />
             </Card>
           </Col>
         </Row>
